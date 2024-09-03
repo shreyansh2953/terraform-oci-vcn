92ad6872e6564bc5890c9c46ec9cf1daffdd9423
Shreyansh <86968197+shreyansh2953@users.noreply.github.com>
2024-09-03 13:03:45 +0530

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -3,7 +3,7 @@ resource "oci_core_vcn" "test_vcn" {
     compartment_id = var.compartment_id
     cidr_blocks = var.vcn_cidr_blocks
     defined_tags = var.defined_tags
-    display_name = var.vcn_name
+    display_name = "${var.vcn_name}-test-orm"
     dns_label = var.dns_label
     freeform_tags = var.freeform_tags
 }
